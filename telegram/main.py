699caf2fadba97ddac35469bb543ef808060b7cb
Leo <leandro.martin@protonmail.com>
2024-10-16 15:50:02 -0300

    Update main.py

--- telegram/main.py
+++ telegram/main.py
@@ -1,13 +1,15 @@
 import telebot
 import datetime
 import os
+import asyncio
 from jinja2 import Environment, FileSystemLoader
+import random
 
 from utils.reader import reader
 from utils.preprocesser import sort_by
 from utils.ranker import ranker
 
-
+rate_limiter = asyncio.Semaphore(20)
 FILEPATH = './data.json'
 today = str(datetime.datetime.today().strftime('%Y-%m-%d'))
 
@@ -19,32 +21,44 @@ chat_id = os.getenv('TELEGRAM_CHANNEL')
 bot = telebot.TeleBot(bot_token)
 
 # Function to send a message
-def send_message(message):
-    try:
-        bot.send_message(chat_id, message)
-        print("Message sent successfully!")
-    except Exception as e:
-        print("Error sending message:", str(e))
+async def send_message(message):
+    async with rate_limiter:
+        try:
+            if len(message) > 4095:
+                for x in range(0, len(message), 4095):
+                    bot.send_message(chat_id, message[x:x+4095])
+            else:
+                bot.send_message(chat_id, message)
+            print("Message sent successfully!")
+        except telebot.apihelper.ApiTelegramException as e:
+            if e.error_code == 429:  # Rate limit exceeded
+                wait_time = random.randint(1, 60)  # Wait for 1 to 60 seconds
+                print(f"Rate limit exceeded. Waiting {wait_time} seconds...")
+                await asyncio.sleep(wait_time)
+                await send_message(message)  # Retry the request
+            else:
+                raise e
 
 # Example usage
 if __name__ == "__main__":
     data = reader(FILEPATH)
-    data = ranker(data, threshold=0)
+    data = ranker(data, threshold=0, date=today)
     data = sort_by(data)
 
     env = Environment(loader=FileSystemLoader('templates'))
 
     xResume = env.get_template('notePost.j2')
     resume_page = xResume.render(today=today)
-    send_message(resume_page)
+    if data != []:
+        asyncio.run(send_message(resume_page))
 
     types = set([x['type'] for x in data])
     for t in types:
         xPostTemplate = env.get_template('head.j2')
         head = xPostTemplate.render(today=today, type=t)
-        send_message(head)
+        asyncio.run(send_message(head))
         for x in data:
             if t == x['type']:
                 xPostTemplate = env.get_template('post.j2')
                 post = xPostTemplate.render(publication=x, today=today, type=x['type'])
-                send_message(post)
+                asyncio.run(send_message(post))
